1c8b60c6084982b9046575248532fa8525ef188e
ngujimmy <ngujimmy@unc.edu>
2022-08-22 21:16:31 -0400

    Fixing up errors #1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,12 +1,11 @@
-"""EX01 - Chardle"""
+"""EX01 - Chardle - A cute step towards Wordle."""
 
 __author__ = "730574005"
 
 five_word: str = input("Enter a 5-character word: ")
 if len(five_word) != 5:
-    print("Error: Word must contain 5 characters")
-    exit()
-
+    exit(print("Error: Word must contain 5 characters"))
+    
 single_letter: str = input("Enter a single character: ")
 instances: int = 0
 print("Searching for " + single_letter + " in " + five_word)
@@ -26,9 +25,9 @@ if five_word[3] == single_letter:
 if five_word[4] == single_letter:
     print(single_letter + " found at index 4")
     instances = instances + 1
-if instances >= 1:
+if instances == 1:
+    print(str(instances) + " instance of " + single_letter + " found in " + five_word)
+elif instances > 1:
     print(str(instances) + " instances of " + single_letter + " found in " + five_word)
 else:
     print("No instances of " + single_letter + " found in " + five_word)
-
-
