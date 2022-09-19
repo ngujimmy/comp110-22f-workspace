d07fb28dd0c8afe63b337ca0efab7d6beab2de3d
ngujimmy <ngujimmy@unc.edu>
2022-09-19 14:54:43 -0400

    max function complete

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,6 +1,7 @@
 """EX04 List."""
 __author__ = "730574005"
 
+
 def all(int_list: list, test: int) -> bool:
     i: int = 0
     match: int = test
@@ -16,6 +17,21 @@ def all(int_list: list, test: int) -> bool:
             i += 1
     if result == True:
         print(True)
+        return True
     else:
         print(False)
+        return False
+
+
+def max(int_list: list) -> int:
+    i: int = 0
+    max: int = int_list[0]
+    while i > len(int_list):
+        if int_list[i] > max:
+            max = int_list[i]
+            i += 1
+        i += 1
+    print(max)
+    return(max)
+
 
